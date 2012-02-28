c2cdd1f385d99ab1dea954d22efe1767a9db78a4
Eloy Duran <eloy.de.enige@gmail.com>
2012-02-28 18:18:18 +0100

    Get rid of global Counter completely.

--- lib/mac_bacon.rb
+++ lib/mac_bacon.rb
@@ -10,10 +10,13 @@
 framework "Cocoa"
 require "mac_bacon/helpers"
 
+# We need to use Kernel::print when printing which specification is being run.
+# But we want to know this as soon as possible, hence we need to sync.
+$stdout.sync = true
+
 module Bacon
   VERSION = "1.3"
 
-  Counter = Hash.new(0)
   Shared = Hash.new { |_, name|
     raise NameError, "no such context: #{name.inspect}"
   }
@@ -24,19 +27,23 @@ module Bacon
   Backtraces = true  unless defined? Backtraces
 
   module SpecDoxOutput
-    def handle_specification_begin(name)
-      puts spaces + name
+    def handle_context_begin(context)
+      spaces = "  " * (context.context_depth - 1)
+      puts spaces + context.name
     end
 
-    def handle_specification_end
-      puts if Counter[:context_depth] == 1
+    def handle_context_end(context)
+      # TODO we should only add an extra line if the next context is on level one
+      # This means that nested contexts do _not_ have an extra line between them and their parent.
+      puts #if context.context_depth == 1
     end
 
-    def handle_requirement_begin(description)
-      print "#{spaces}  - #{description}"
+    def handle_specification_begin(specification)
+      spaces = "  " * (specification.context.class.context_depth - 1)
+      print "#{spaces}  - #{specification.description}"
     end
 
-    def handle_requirement_end(error)
+    def handle_specification_end(error)
       puts error.empty? ? "" : " [#{error}]"
     end
 
@@ -54,18 +61,14 @@ module Bacon
       errors = Specification.specifications.select(&:error?).size
       puts "%d specifications (%d requirements), %d failures, %d errors" % [specs, reqs, failed, errors]
     end
-
-    def spaces
-      "  " * (Counter[:context_depth] - 1)
-    end
   end
 
   #module TestUnitOutput
-    #def handle_specification_begin(name); end
-    #def handle_specification_end        ; end
+    #def handle_context_begin(name); end
+    #def handle_context_end        ; end
 
-    #def handle_requirement_begin(description) end
-    #def handle_requirement_end(error)
+    #def handle_specification_begin(description) end
+    #def handle_specification_end(error)
       #if error.empty?
         #print "."
       #else
@@ -82,14 +85,14 @@ module Bacon
   #end
 
   #module TapOutput
-    #def handle_specification_begin(name); end
-    #def handle_specification_end        ; end
+    #def handle_context_begin(name); end
+    #def handle_context_end        ; end
 
-    #def handle_requirement_begin(description)
+    #def handle_specification_begin(description)
       #ErrorLog.replace ""
     #end
 
-    #def handle_requirement_end(error)
+    #def handle_specification_end(error)
       #if error.empty?
         #puts "ok %-3d - %s" % [Counter[:specifications], description]
       #else
@@ -107,14 +110,14 @@ module Bacon
   #end
 
   #module KnockOutput
-    #def handle_specification_begin(name); end
-    #def handle_specification_end        ; end
+    #def handle_context_begin(name); end
+    #def handle_context_end        ; end
 
-    #def handle_requirement_begin(description)
+    #def handle_specification_begin(description)
       #ErrorLog.replace ""
     #end
 
-    #def handle_requirement_end(error)
+    #def handle_specification_end(error)
       #if error.empty?
         #puts "ok - %s" % [description]
       #else
@@ -152,7 +155,7 @@ module Bacon
       end
     end
 
-    attr_reader :description
+    attr_reader :description, :context
 
     def initialize(context, description, block, before_filters, after_filters)
       @context, @description, @block = context, description, block
@@ -187,11 +190,10 @@ module Bacon
 
     def run
       # TODO this should probably be done differently in a parallel setup
-      Bacon.handle_requirement_begin(@description)
-      Counter[:depth] += 1
+      Bacon.handle_specification_begin(self)
 
       run_before_filters
-      @number_of_requirements_before = Counter[:requirements]
+      @number_of_requirements_before = Should.requirements.size
       run_spec_block unless postponed?
     end
 
@@ -279,7 +281,7 @@ module Bacon
     end
 
     def finish_spec
-      if passed? && Counter[:requirements] == @number_of_requirements_before
+      if passed? && Should.requirements.size == @number_of_requirements_before
         # the specification did not contain any requirements, so it flunked
         execute_block { raise Error.new(:missing, "empty specification: #{@context.class.name} #{@description}") }
       end
@@ -294,8 +296,7 @@ module Bacon
 
     def exit_spec
       cancel_scheduled_requests!
-      Counter[:depth] -= 1
-      Bacon.handle_requirement_end(error_message || '')
+      Bacon.handle_specification_end(error_message || '')
       @context.class.specification_did_finish(self)
     end
 
@@ -358,22 +359,20 @@ module Bacon
 
   def self.run
     @timer ||= Time.now
-    Counter[:context_depth] += 1
-    handle_specification_begin(current_context.name)
+    handle_context_begin(current_context)
     current_context.performSelector("run", withObject:nil, afterDelay:0)
     NSApplication.sharedApplication.run
   end
 
   def self.context_did_finish(context)
-    handle_specification_end
-    Counter[:context_depth] -= 1
+    handle_context_end(context)
     if (@current_context_index + 1) < @contexts.size
       @current_context_index += 1
       run
     else
       # DONE
       handle_summary
-      exit(Counter.values_at(:failed, :errors).inject(:+))
+      exit Specification.specifications.select { |s| !s.passed? }.size
     end
   end
 
@@ -383,7 +382,7 @@ module Bacon
     def change?(*args, &block); block.change?(*args); end
 
     def should(*args, &block)
-      if Counter[:depth]==0
+      if self.class.context_depth == 0
         it('should '+args.first,&block)
       else
         super(*args,&block)
@@ -411,14 +410,15 @@ module Bacon
     end
 
     class << self
-      attr_reader :name, :block
+      attr_reader :name, :block, :context_depth
 
-      def init_context(name, before = nil, after = nil, &block)
+      def init_context(name, context_depth, before = nil, after = nil, &block)
         context = Class.new(self) do
           @name = name
           @before, @after = (before ? before.dup : []), (after ? after.dup : [])
           @block = block
           @specifications = []
+          @context_depth = context_depth
           @current_specification_index = 0
         end
         Bacon.add_context(context)
@@ -459,13 +459,12 @@ module Bacon
       def it(description, &block)
         return  unless description =~ RestrictName
         block ||= lambda { should.flunk "not implemented" }
-        Counter[:specifications] += 1
         @specifications << Specification.new(new, description, block, @before, @after)
       end
 
       def describe(*args, &block)
         args.unshift(name)
-        init_context(args.join(' '), @before, @after, &block)
+        init_context(args.join(' '), @context_depth + 1, @before, @after, &block)
       end
     end
   end
@@ -518,13 +517,13 @@ end
 
 
 class Object
-  def should(*args, &block)    Should.new(self).be(*args, &block)                  end
+  def should(*args, &block)    Should.new(self).be(*args, &block)                     end
 end
 
 module Kernel
   private
-  def describe(*args, &block) Bacon::Context.init_context(args.join(' '), &block)  end
-  def shared(name, &block)    Bacon::Shared[name] = block                          end
+  def describe(*args, &block) Bacon::Context.init_context(args.join(' '), 1, &block)  end
+  def shared(name, &block)    Bacon::Shared[name] = block                             end
 end
 
 class Should
@@ -574,13 +573,13 @@ class Should
     end
 
     r = yield(@object, *args)
-    if Bacon::Counter[:depth] > 0
-      Bacon::Counter[:requirements] += 1
+    # TODO not sure if and how we should fix this
+    #if Bacon::Counter[:depth] > 0
       raise Bacon::Error.new(:failed, description)  unless @negated ^ r
       r
-    else
-      @negated ? !r : !!r
-    end
+    #else
+      #@negated ? !r : !!r
+    #end
   end
 
   def method_missing(name, *args, &block)
